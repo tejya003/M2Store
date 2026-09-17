c56e25524b7ff8a4edab3c9b07193035ee7503d4
tejas <tejasgaikwad.tech@gmail.com>
2026-09-17 16:00:01 +0530

    Remove debug endpoint

--- backend/server.js
+++ backend/server.js
@@ -28,10 +28,6 @@ app.get('/', (req, res) => {
   res.send('API is running');
 });
 
-app.get('/debug-key', (req, res) => {
-  res.send('Key length: ' + (process.env.BREVO_API_KEY || 'NOT SET').length + ' | Starts with: ' + (process.env.BREVO_API_KEY || 'NOT SET').substring(0, 15));
-});
-
 app.use('/api/products', productRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
