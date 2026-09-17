2d31e1ad4b14d6858d14baa400d539dedfc26624
tejas <tejasgaikwad.tech@gmail.com>
2026-09-17 12:55:30 +0530

    Use Brevo HTTP API instead of SMTP for email

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,22 +1,21 @@
-const nodemailer = require('nodemailer');
-
-const transporter = nodemailer.createTransport({
-  host: 'smtp-relay.brevo.com',
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.BREVO_SMTP_LOGIN,
-    pass: process.env.BREVO_SMTP_KEY
-  }
-});
+const axios = require('axios');
 
 const sendEmail = async (to, subject, text) => {
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
-    to,
-    subject,
-    text
-  });
+  await axios.post(
+    'https://api.brevo.com/v3/smtp/email',
+    {
+      sender: { email: process.env.EMAIL_USER, name: 'M2 Store' },
+      to: [{ email: to }],
+      subject: subject,
+      textContent: text
+    },
+    {
+      headers: {
+        'api-key': process.env.BREVO_API_KEY,
+        'Content-Type': 'application/json'
+      }
+    }
+  );
 };
 
 module.exports = sendEmail;
